d937ef21ad4fb487817b2d20596f164d26c4c6a0
Hectorcg88 <casesgimenezh@gmail.com>
2024-02-16 16:34:13 +0100

    asds

---
+++ prueba.php
@@ -0,0 +1,11 @@
+<?php
+// Recibe los valores a sumar desde la URL
+$valor1 = $_GET['valor1'] ?? 0;
+$valor2 = $_GET['valor2'] ?? 0;
+
+// Realiza la suma
+$suma = $valor1 + $valor2;
+
+// Devuelve el resultado
+echo "La suma de $valor1 y $valor2 es: $suma";
+?>
